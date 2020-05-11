00920975625c39c0644b7e6aec74bc06c34feaae
orange_kokubun <kazup1125@me.com>
2020-05-11 22:10:15 +0900

    Add: Categoryモデル,OtakuWordモデルのアソシエーション

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :category_and_otaku_word_relationships
+  has_many :otaku_words, through: :category_and_otaku_word_relationships
 end
--- app/models/otaku_word.rb
+++ app/models/otaku_word.rb
@@ -1,2 +1,4 @@
 class OtakuWord < ApplicationRecord
+  has_many :category_and_otaku_word_relationships
+  has_many :categorys, through: :category_and_otaku_word_relationships
 end
