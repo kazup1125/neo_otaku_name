b199e6b8386443a1fce0d953cb9b66ecb7ba03ac
orange_kokubun <kazup1125@me.com>
2020-06-05 22:11:56 +0900

    Fix: spec config.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,15 @@ module NeoOtakuWord
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # $ rails g コマンドでrspecようのテストファイルを作成するための設定
+    g.test_framework :rspec,
+                     fixtures: true,
+                     view_specs: false,
+                     helper_specs: false,
+                     routing_specs: false,
+                     controller_specs: true,
+                     request_specs: false
+    g.fixture_replacement :factory_bot, dir: "spec/factories"
   end
 end
