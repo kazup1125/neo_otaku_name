dfb8725d3eb8f58a7f4a199799a657078635e9e7
orange_kokubun <kazup1125@me.com>
2020-06-05 22:15:30 +0900

    Fix: タイポ

--- config/application.rb
+++ config/application.rb
@@ -17,13 +17,15 @@ module NeoOtakuWord
     # the framework and any gems in your application.
 
     # $ rails g コマンドでrspecようのテストファイルを作成するための設定
-    g.test_framework :rspec,
-                     fixtures: true,
-                     view_specs: false,
-                     helper_specs: false,
-                     routing_specs: false,
-                     controller_specs: true,
-                     request_specs: false
-    g.fixture_replacement :factory_bot, dir: "spec/factories"
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end 
   end
 end
