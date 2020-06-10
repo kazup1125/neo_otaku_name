205dd9f17c8c337dfea3e6cd785bf64446ef80b8
orange_kokubun <kazup1125@me.com>
2020-06-10 23:04:53 +0900

    Update: 管理画面からオタク用語のCRUD処理を行う時に、関連するカテゴリも一緒に処理するように変更

--- app/admin/otaku_words.rb
+++ app/admin/otaku_words.rb
@@ -5,7 +5,8 @@ ActiveAdmin.register OtakuWord do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :word, :meaning
+  permit_params :word, :meaning,
+                category_and_otaku_word_relationships_attributes: [:id, :category_id, :_destroy]
   #
   # or
   #
@@ -14,5 +15,18 @@ ActiveAdmin.register OtakuWord do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+  form do |f|
+    f.inputs do
+      f.input :word
+      f.input :meaning
+      f.has_many :category_and_otaku_word_relationships, allow_destroy: true do |r|
+        r.input :category,
+                label: 'カテゴリー',
+                as: :select,
+                collection: Category.all
+      end
+      f.actions
+    end
+  end
   
 end
--- app/models/otaku_word.rb
+++ app/models/otaku_word.rb
@@ -1,4 +1,6 @@
 class OtakuWord < ApplicationRecord
   has_many :category_and_otaku_word_relationships
-  has_many :categorys, through: :category_and_otaku_word_relationships
+  has_many :categorys, through: :category_and_otaku_word_relationships, dependent: :destroy
+
+  accepts_nested_attributes_for :category_and_otaku_word_relationships, allow_destroy: true
 end
