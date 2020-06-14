82cee37e6b17f7b8207507391c22a381c9a99514
orange_kokubun <kazup1125@me.com>
2020-06-14 22:41:48 +0900

    Update: オタク用語一覧の行をクリックすると、そのオタク用語の詳細画面へ遷移するように変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("channels")
 require("jquery")
 import 'bootstrap'
 import '../src/application.scss'
-
+import '../custom/click_row'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
