fd4d180fae439753d4c6094ae0ed28200872d764
orange_kokubun <kazup1125@me.com>
2020-06-20 14:58:10 +0900

    Add: トップのモック作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/top'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root 'otaku_words#index'
 
+  root to: "home#top"
+
+  get 'home/top'
   resources :otaku_words, only: [:index, :show]
   resources :categories, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
