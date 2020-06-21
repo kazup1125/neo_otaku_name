5fd984920ebdf3c461383a395c4db2d810fcda7e
orange_kokubun <kazup1125@me.com>
2020-06-21 19:52:48 +0900

    Add: ja.yml

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,6 +9,8 @@ module NeoOtakuWord
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
