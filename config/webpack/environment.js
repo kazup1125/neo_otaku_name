ff57cec1fe9815ad9b79c7d7df0e31ed0dec7a59
orange_kokubun <kazup1125@me.com>
2020-06-22 03:06:18 +0900

    Fix: 遷移しない問題

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,12 @@
 const { environment } = require('@rails/webpacker')
-var webpack = require('webpack');
-
-environment.plugins.append(
-    'Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery',
-    })
-)
 
 module.exports = environment
+
+const webpack = require('webpack')
+
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
