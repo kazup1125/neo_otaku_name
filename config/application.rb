6c5d7e1954141d7252129b16acd81181d01d3605
orange_kokubun <kazup1125@me.com>
2020-07-13 22:38:36 +0900

    Add: 環境変数を設定

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
 module NeoOtakuWord
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
