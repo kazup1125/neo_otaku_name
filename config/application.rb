7809891701d36ae178c3f735ec855c7217431881
orange_kokubun <kazup1125@me.com>
2020-07-15 22:00:24 +0900

    Update: $rails gコマンドで生成されるテストファイルを追加

--- config/application.rb
+++ config/application.rb
@@ -23,11 +23,11 @@ module NeoOtakuWord
     config.generators do |g|
       g.test_framework :rspec,
                        fixtures: true,
-                       view_specs: false,
-                       helper_specs: false,
-                       routing_specs: false,
+                       view_specs: true,
+                       helper_specs: true,
+                       routing_specs: true,
                        controller_specs: true,
-                       request_specs: false
+                       request_specs: true
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end 
   end
