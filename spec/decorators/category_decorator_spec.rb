7f08cbe9dacac6f0d608b125c0d2d8687a872214
orange_kokubun <kazup1125@me.com>
2020-07-16 10:07:53 +0900

    Fix: spec to category_decorator.

--- spec/decorators/category_decorator_spec.rb
+++ spec/decorators/category_decorator_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 RSpec.describe CategoryDecorator do
   let(:category) { Category.new.extend CategoryDecorator }
-  subject { category }
-  it { should be_a Category }
+  subject { category.categories_badge }
+  it 'categories_badgeメソッドを使うと、badge badge-secondaryを返す' do
+    is_expected.to eq 'badge badge-secondary'
+  end
 end
