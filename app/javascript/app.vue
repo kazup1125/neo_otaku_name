0066c37d2fbf8caf0fcf77150329036cbdb3c0ff
orange_kokubun <kazup1125@me.com>
2020-07-31 17:35:59 +0900

    Add: Vueで一覧の項目を表示させた

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <table>
+      <tbody>
+        <tr>
+          <th>NO.</th>
+          <th>オタク語</th>
+          <th>標準語</th>
+        </tr>
+        <tr v-for="w in otaku_words" :key="w.id">
+          <td>{{ w.id }}</td>
+          <td>{{ w.word }}</td>
+          <td>{{ w.meaning }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
+  import axios from 'axios';
+  export default {
+    data: function () {
+      return {
+        otaku_words: []
+      }
+    },
+    mounted() {
+      axios
+      .get('/api/v1/otaku_words.json')
+      .then(response => (this.otaku_words = response.data))
     }
   }
-}
 </script>
 
 <style scoped>
