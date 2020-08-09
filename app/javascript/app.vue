bab9ebae35486c6dd3c27a99ce34508268f3c24b
orange_kokubun <kazup1125@me.com>
2020-08-09 22:29:15 +0900

    Add: 変換ボタンを押すと、オタク用語の0番目の意味と解説を表示する

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,19 +1,28 @@
 <template>
-  <div id="app">
-    <table>
-      <tbody>
-        <tr>
-          <th>NO.</th>
-          <th>オタク語</th>
-          <th>標準語</th>
-        </tr>
-        <tr v-for="w in otaku_words" :key="w.id">
-          <td>{{ w.id }}</td>
-          <td>{{ w.word }}</td>
-          <td>{{ w.meaning }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div id="app" class="container">
+    <div class="row justify-content-center">
+      <h3>オタク用語→標準語に変換する</h3>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <input id="word" type="text" v-model="inputWord" class="form-control" placeholder="オタク用語を入力してください">
+      </div>
+      <button type="button" class="btn btn-primary" @click="convertIntoStandardWord">変換</button>
+    </div>
+      <br><br>
+    <div class="row justify-content-center">
+      <h3>変換結果</h3>
+      <br><br>
+    </div>
+    <div class="row justify-content-center">
+      <h4 class="text-primary">{{standard}}</h4>
+    </div>
+    <div class="row justify-content-center">
+      <p class="text-primary">{{description}}</p>
+    </div>
+    <div class="row justify-content-center">
+      <button type="button" class="btn btn-warning" @click="clearDisplay">クリア</button>
+    </div>
   </div>
 </template>
 
@@ -22,20 +31,33 @@
   export default {
     data: function () {
       return {
-        otaku_words: []
+        inputWord: '',
+        standard: '',
+        description: '',
+        otakuWords: []
       }
     },
     mounted() {
       axios
       .get('/api/v1/otaku_words.json')
-      .then(response => (this.otaku_words = response.data))
+      .then(response => {
+        this.otakuWords = response.data
+        console.log(response.data)
+      });
+    },
+    methods: {
+      convertIntoStandardWord(){
+        // TODO 検索機能
+        this.standard = this.otakuWords[0].meaning
+        this.description = this.otakuWords[0].description
+      },
+      clearDisplay(){
+        this.standard = ''
+        this.description = ''
+      }
     }
   }
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+<style scoped lang="scss">
 </style>
