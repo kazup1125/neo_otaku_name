0d05e0929fcffae1197f70564bbe7785374b2ee9
orange_kokubun <kazup1125@me.com>
2020-12-06 00:10:05 +0900

    Add: deviseのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'home#top'
   post '/callback' => 'line_bot#callback'
 
+  devise_for :admin_users
+
   namespace :api, {format: 'json'} do
     namespace :v1 do
       resources :otaku_words, only: [:index, :show]
